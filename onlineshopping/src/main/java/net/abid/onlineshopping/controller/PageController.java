6180cb28a062600c02042ccbff9e2de406d584b3
Abid.Imran <abidimran003@gmail.com>
2018-05-13 22:16:23 +0530

    Successful Loaded Static resources

--- onlineshopping/src/main/java/net/abid/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/abid/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.abid.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,6 +17,29 @@ public class PageController {
 		mv.addObject("greeting","Welcome to Spring MVC");
 		return mv;
 		
+	}
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false )String greeting)
+	{
+		if(greeting == null)
+		{
+			greeting="Hello Null Value";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+	}*/
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting", required=false )String greeting)
+	{
+		if(greeting == null)
+		{
+			greeting="Hello Null Value";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
 	}
 
 }
